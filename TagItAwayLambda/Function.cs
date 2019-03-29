504713f8a3715d6f2d5445d240b03cbf150e58db
Sergiu.Gandrabura <Sergiu.Gandrabura@groupimd.com>
2019-03-29 09:53:14 +0000

    Changed the Files Names fior Lambda

--- TagItAwayLambda/Function.cs
+++ TagItAwayLambda/Function.cs
@@ -12,7 +12,7 @@ using Amazon.S3.Model;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
-namespace SchnitzelOrNotLambda
+namespace TagItAwayLambda
 {
     public class Function
     {
